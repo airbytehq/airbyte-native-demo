1ea0fc1ff75cb7be31926420bdc01775ea987f68
Brian Leonard <brian@bleonard.com>
2023-04-17 16:27:14 -0700

    longer timeout

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -6,7 +6,7 @@ export const HOST_URL = "https://api.airbyte.com";
 export function getClient(user?: CurrentUser): AxiosInstance {
   const info: CreateAxiosDefaults = {
     baseURL: HOST_URL,
-    timeout: 5000,
+    timeout: 10000,
     headers: {},
   };
 
