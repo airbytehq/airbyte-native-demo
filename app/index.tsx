8d58723487fbd946cd64a26db5c311021604e43b
Brian Leonard <brian@bleonard.com>
2023-05-15 13:50:05 -0700

    adds logo

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,10 @@ import { useRouter } from "expo-router";
 import { useEffect } from "react";
 import { autoloadOnStartup } from "../lib/api/login";
 import { useAuth } from "../lib/context/auth";
-import { Text } from "@rneui/themed";
+import { Image } from "@rneui/themed";
 import { useProgress } from "../lib/context/progress";
 import { Container } from "../lib/components/Container";
+import { Dimensions, StyleSheet, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
@@ -28,8 +29,32 @@ export default function Index() {
   }, []);
 
   return (
-    <Container>
-      <Text>Airbyte Logo</Text>
+    <Container title=" ">
+      <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={require("../assets/airbyte_logo.png")}
+        />
+      </View>
     </Container>
   );
 }
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+const imageWidth = Math.min(windowWidth - 40, 500);
+const imageHeight = (200 / 738.0) * imageWidth;
+const toolbarHeight = 56;
+const aboveHeight =
+  (windowHeight - imageHeight) / 2 - imageHeight / 2 - toolbarHeight;
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    marginTop: aboveHeight,
+    width: imageWidth,
+    height: imageHeight,
+  },
+});
